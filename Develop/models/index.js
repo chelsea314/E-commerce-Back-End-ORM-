bbd1c7cd0652151fcfcc2f995d163e8e09249ede
Chelsea Sexton <chelseansexton@gmail.com>
2022-08-11 16:34:38 -0500

    index.js updated

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,17 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  // When we delete a category, make sure to also delete the associated products.
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag,{
-  
-})
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: {
+      model: ProductTag,
+      unique: false
+    },
+});
 
 
 module.exports = {
